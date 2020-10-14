fa51b24d87a766dfaad0e71af190510be3d70442
rtetelbaum <rtetelbaum@mailbox.org>
2020-10-14 14:40:08 -0400

    initial commit

---
+++ app/controllers/doctor_controller.rb
@@ -0,0 +1,13 @@
+class DoctorController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'doctor/index'
+  get 'doctor/show'
+  get 'doctor/new'
+  get 'doctor/edit'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
